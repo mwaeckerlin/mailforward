f37f0f6b8ff55a775a92adbd5e98167b5b983b65
Marc Wäckerlin <marc@waeckerlin.org>
2015-06-27 16:02:19 +0200

    small fixes

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -ex
 
 postconf -e virtual_alias_domains="${DOMAINS}"
+postconf -e mydestination="${DOMAINS}"
 echo "${MAPPINGS}" | sed 's/, */\n/g' > /etc/postfix/virtual
 postmap /etc/postfix/virtual
+service rsyslog start
 service postfix restart
 sleep infinity
-
