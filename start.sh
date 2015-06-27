4bd8eed6c1d5aaaa0cc789c34e641894f5cd9d06
Marc Wäckerlin <marc@waeckerlin.org>
2015-06-27 15:32:10 +0200

    generate virtual map

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 postconf -e virtual_alias_domains="${DOMAINS}"
-postconf -e /etc/postfix/virtual="$(echo $MAPPINGS | sed 's/, */\n/g')"
+echo "${MAPPINGS}" | sed 's/, */\n/g')" > /etc/postfix/virtual
 postmap /etc/postfix/virtual
 service postfix restart
 sleep infinity
