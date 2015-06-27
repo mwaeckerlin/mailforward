3b7789ee7b3304bf597d94116b20e47d100064b6
Marc Wäckerlin <marc@waeckerlin.org>
2015-06-27 15:39:12 +0200

    typo

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 postconf -e virtual_alias_domains="${DOMAINS}"
-echo "${MAPPINGS}" | sed 's/, */\n/g')" > /etc/postfix/virtual
+echo "${MAPPINGS}" | sed 's/, */\n/g' > /etc/postfix/virtual
 postmap /etc/postfix/virtual
 service postfix restart
 sleep infinity
