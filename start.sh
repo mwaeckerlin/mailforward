5bf8213200d0aecac8ed5d86a4dc570340158555
Marc Wäckerlin <marc@waeckerlin.org>
2015-07-01 10:31:01 +0200

    added virtual_alias_maps=hash:/etc/postfix/virtual

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 postconf -e virtual_alias_domains="${DOMAINS}"
 postconf -e mydestination="${DOMAINS}"
+postconf -e "virtual_alias_maps=hash:/etc/postfix/virtual"
 echo "${MAPPINGS}" | sed 's/, */\n/g' > /etc/postfix/virtual
 postmap /etc/postfix/virtual
-service rsyslog start
 service postfix restart
 sleep infinity
